9790510d303bd1bf38e1b07c9958a4fb322a3651
k1te <kmiller343@gmail.com>
2020-11-12 14:19:17 -0600

    rewrote some of the signup functionality, working

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -1,32 +1,34 @@
 $(document).ready(() => {
-    // Form & Input References
-    const signUpForm = $("#signup");
-    const userName = $("#userName");
-    const emailInput = $("#email");
-    const passwordInput = $("#password")
+  // Form & Input References
+  const signUpForm = $("#signup");
+  const userName = $("#userName");
+  const emailInput = $("#email");
+  const passwordInput = $("#password");
 
-    signUpForm.on("submit", event => {
-        event.preventDefault();
-        const userData = {
-            name: userName.val().trim(),
-            email: emailInput.val().trim(),
-            password: passwordInput.val().trim()
-        };
-    
-        signUp(userData.name, userData.email, userData.password);
-        userName.val("");
-        emailInput.val("");
-        passwordInput.val("");
-    })
+  signUpForm.on("submit", (event) => {
+    event.preventDefault();
+    const userData = {
+      name: userName.val().trim(),
+      email: emailInput.val().trim(),
+      password: passwordInput.val().trim(),
+    };
+    console.log(userData);
 
-    function signUp(userName, email, password) {
-        $.post("/api/signup", {
-            name: userName,
-            email: email,
-            password: password
-        })
-         .then(() => {
-             window.location.replace("/");
-         })
-    }
-})
+    signUp(userData.name, userData.email, userData.password);
+    userName.val("");
+    emailInput.val("");
+    passwordInput.val("");
+  });
+
+  function signUp(userName, email, password) {
+    let obj = {
+      name: userName,
+      email: email,
+      password: password,
+    };
+    console.log(obj);
+    $.post("/api/signup", obj).then(() => {
+      window.location.replace("/");
+    });
+  }
+});
