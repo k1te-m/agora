8efcb32ac7d39779df3f067be30c0f29efe67c02
k1te <kmiller343@gmail.com>
2020-11-12 12:51:41 -0600

    formatted document

--- models/User.js
+++ models/User.js
@@ -3,48 +3,47 @@ const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
 const SALT_WORK_FACTOR = 10;
 
-const UserSchema = new Schema ({
-    name: {
-        type: String,
-        trim: true,
-        required: `Please enter your name.`
-    },
-    email: {
-        type: String,
-        trim: true,
-        unique: true,
-        required: `Email address is required.`,
-        match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
-    },
-    password: {
-        type: String,
-        trim: true,
-        required: `Please enter a password.`
-    },
-})
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: `Please enter your name.`,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: `Email address is required.`,
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: `Please enter a password.`,
+  },
+});
 
 UserSchema.pre("save", () => {
-    var user = this;
-    if (!user.isModified("password")) return next();
+  var user = this;
+  if (!user.isModified("password")) return next();
 
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(error, salt) {
-        if (error) return next(error);
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (error, salt) {
+    if (error) return next(error);
 
-        bcrypt.hash(user.password, salt, function(error, hash) {
-            if (error) return next(error);
-            user.password = hash;
-            next();
-        })
-    })
-})
+    bcrypt.hash(user.password, salt, function (error, hash) {
+      if (error) return next(error);
+      user.password = hash;
+      next();
+    });
+  });
+});
 
-UserSchema.methods.comparePassword = function(userPassword, cb) {
-    bcrypt.compare(userPassword, this.password, function(error, isMatch) {
-        if (error) return cb(error);
-        cb(null, isMatch);
-    })
-}
-
+UserSchema.methods.comparePassword = function (userPassword, cb) {
+  bcrypt.compare(userPassword, this.password, function (error, isMatch) {
+    if (error) return cb(error);
+    cb(null, isMatch);
+  });
+};
 
 const User = mongoose.model("User", UserSchema);
 
