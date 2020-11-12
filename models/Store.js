8ae1cf8b6058ec0191ac7fc37d4650c4ac0452bd
k1te <kmiller343@gmail.com>
2020-11-12 00:25:45 -0600

    formatted doc

--- models/Store.js
+++ models/Store.js
@@ -2,21 +2,21 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const StoreSchema = new Schema ({
-    name: {
-        type: String,
-        trim: true,
-        required: `Please enter a name for your store.`
-    },
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-    },
-    products: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Product"
-    }
-})
+const StoreSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: `Please enter a name for your store.`,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  products: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+  },
+});
 
 const Store = mongoose.model("Store", StoreSchema);
 
