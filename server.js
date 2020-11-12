a49360b18630fc646009a5886f66a1984d17998d
k1te <kmiller343@gmail.com>
2020-11-12 00:16:08 -0600

    included apiRoutes for server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const routes = require("./routes/html-routes");
+const apiRoutes = require("./routes/api-routes");
 
 // PORT
 const PORT = process.env.PORT || 3000;
@@ -30,6 +31,7 @@ app.set("view engine", "handlebars");
 
 // Routes
 app.use(routes);
+app.use(apiRoutes);
 
 // Listen on PORT
 app.listen(PORT, function () {
