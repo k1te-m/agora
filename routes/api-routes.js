85920ba8c15d7fa34d4278eddbffd167c09b428d
k1te <kmiller343@gmail.com>
2020-11-12 15:08:12 -0600

    added api/login route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const db = require("../models/index");
 const apiRouter = express.Router();
+const passport = require("../config/passport");
 
 apiRouter.post("/api/signup", (req, res) => {
     console.log(req.body);
@@ -9,12 +10,19 @@ apiRouter.post("/api/signup", (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-      .then((newUser) => {
-          res.json(newUser);
+      .then(() => {
+          res.redirect(307, "/")
       })
       .catch((error) => {
-          res.json(error);
+          res.status(401).json(error);
       })
 })
+
+apiRouter.post("/api/login", passport.authenticate("local"), (req, res) => {
+    res.json({
+        email: req.user.email,
+        id: req.user.id
+    });
+});
 
 module.exports = apiRouter;
