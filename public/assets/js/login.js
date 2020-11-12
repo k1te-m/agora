79c25c079249301bdc7ebb0dd229adb2b409d18c
k1te <kmiller343@gmail.com>
2020-11-12 15:08:02 -0600

    removed trim and updated

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,36 +1,35 @@
 $(document).ready(() => {
-    const loginForm = $("#loginForm");
-    const userEmail = $("#userName");
-    const password = $("password");
+  const loginForm = $("#loginForm");
+  const userEmail = $("#userName");
+  const password = $("password");
 
-    loginForm.on("submit", event => {
-        event.preventDefault();
-        const userData = {
-            email: userEmail.val().trim(),
-            password: password.val().trim()
-        };
+  loginForm.on("submit", (event) => {
+    event.preventDefault();
+    const userData = {
+      email: userEmail.val(),
+      password: password.val(),
+    };
+    console.log(userData);
 
-        if (!userData.email || !userData.password) {
-            return;
-        }
-
-        loginUser(userData.email, userData.password);
-        userEmail.val("");
-        password.val("");
-
-    })
-
-    function loginUser(email, password) {
-        $.post("/api/login", {
-            email: email,
-            password: password
-        })
-         .then(() => {
-             window.location.replace("/");
-         })
-         .catch(error => {
-             console.log(error);
-         })
+    if (!userData.email || !userData.password) {
+      return;
     }
 
-})
+    loginUser(userData.email, userData.password);
+    userEmail.val("");
+    password.val("");
+  });
+
+  function loginUser(email, password) {
+    $.post("/api/login", {
+      email: email,
+      password: password,
+    })
+      .then(() => {
+        window.location.replace("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+});
