38492e5612666ba0eb9f68fa730a172b9e510417
k1te <kmiller343@gmail.com>
2020-11-12 21:05:36 -0600

    removed unecessary middleware

--- config/middleware/isAuthenticated.js
+++
@@ -1,9 +0,0 @@
-// Restrict routes if a user is not logged in
-module.exports = function(req, res, next) {
-    // If logged in, continue with the request
-    if(req.user) {
-        return next();
-    }
-    // If not logged in, redirect to login page
-    return res.redirect("/login");
-}
