093b6c6d52f7dbcc44ae0a717910d06902ff8c5e
k1te <kmiller343@gmail.com>
2020-11-13 00:48:16 -0600

    reordered dependencies, authentication works

--- server.js
+++ server.js
@@ -1,52 +1,45 @@
 // Dependencies
-const express = require("express");
-const mongoose = require("mongoose");
-const logger = require("morgan");
 const routes = require("./routes/html-routes");
 const apiRoutes = require("./routes/api-routes");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const express = require("express");
 const passport = require("passport");
-const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
 
+// Express & Session Set-Up
+const app = express();
+app.use(express.static("public"));
+const session = require("express-session");
 // PORT
 const PORT = process.env.PORT || 3000;
-
-// Express
-const app = express();
-app.use(logger("dev"));
+app.use(
+  session({
+    secret: "bazinga",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Static Directory
-app.use(express.static("public"));
-
-// MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/agora", {
-  useNewUrlParser: true,
-});
+app.use(logger("dev"));
 
 // Handlebars Set-up
 const exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Routes
 app.use(routes);
 app.use(apiRoutes);
 
-// Passport
-app.use(
-  session({
-    secret: "bazinga",
-    resave: false,
-    saveUninitialized: true,
-    store: new MongoStore({ mongooseConnection: mongoose.connection})
-  })
-)
-app.use(passport.initialize());
-app.use(passport.session());
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/agora", {
+  useNewUrlParser: true,
+});
 
 // Listen on PORT
 app.listen(PORT, function () {
