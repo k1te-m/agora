f49f3cda00bae9941cf42d8988342d976d94b8a5
k1te <kmiller343@gmail.com>
2020-11-13 00:50:40 -0600

    added routes for login, logout, members. acclvl +

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,16 +1,53 @@
 const express = require("express");
+const { User } = require("../models/index");
 const router = express.Router();
+const db = require("../models/index")
 
 router.get("/", (req, res) => {
-  res.render("index");
+  let accessLevel = 0;
+  if (req.user) accessLevel = req.user.accessLevel;
+  res.render("index", {loggedIn:accessLevel});
+  console.log(req.user);
 });
 
 router.get("/signup", (req, res) => {
-  res.render("signup");
+  let accessLevel = 0;
+  if (req.user) {
+    console.log(req.user);
+    accessLevel = req.user.accessLevel;}
+  res.render("signup", {loggedIn:accessLevel});
 });
 
 router.get("/login", (req, res) => {
-  res.render("login");
+  let accessLevel = 0;
+  if (req.user) accessLevel = req.user.accessLevel;
+  res.render("login", {loggedIn:accessLevel});
 })
 
+router.get("/bazinga", function(req, res) {
+  res.render("bazinga");
+})
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+})
+
+router.get("/members", (req, res) => {
+  if (req.user) {
+    let userName = req.user.name;
+    let userEmail = req.user.email;
+    let joinDate = req.user.joinDate;
+    let accessLevel = req.user.accessLevel;
+
+    res.render("members", {
+      userName,
+      userEmail,
+      joinDate,
+      accessLevel
+    })
+  } else {
+    res.render("login");
+  }
+})
 module.exports = router;
