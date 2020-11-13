8489e32517b8f2836e2eb006e230fa70e33855ca
k1te <kmiller343@gmail.com>
2020-11-13 00:50:06 -0600

    rewrote /api/login post route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,35 +4,27 @@ const apiRouter = express.Router();
 const passport = require("../config/passport");
 
 apiRouter.post("/api/signup", (req, res) => {
-    console.log(req.body);
     db.User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
     })
       .then(() => {
-          res.redirect(307, "/")
+          res.status(200).json({user: req.body})
+          console.log(`Registerd new user, ${req.body.name}.`)
       })
       .catch((error) => {
+          console.log(error);
           res.status(401).json(error);
       })
 })
 
-apiRouter.post("/api/login", (req, res, next) => {
-    passport.authenticate("local", function(error, user, info) {
-        if (err) {
-            return res.status(400).json({errors: error});
-        }
-        if (!user) {
-            return res.status(400).json({errors: "User not found!"});
-        }
-        req.logIn(user, function(error) {
-            if (error) {
-                return res.status(400).json({errors: error});
-            }
-            return res.status(200).json({ success: `Logged in ${user.id}`});
-        })
+apiRouter.post("/api/login", passport.authenticate("local"), (req, res) => {
+    res.json({
+        username: req.user.email,
+        id: req.user._id
     });
+    console.log(`Logged in as: ${req.user.name}.`);
 })
 
 
